890c0c5c69509214b0b549ea4a39ce722b01c382
Lucas Colley <lucas.colley8@gmail.com>
2025-02-05 15:03:06 +0000

    initial `Dimension` protocol

---
+++ src/dimension_api/__init__.py
@@ -0,0 +1,13 @@
+"""Dimension API."""
+
+from typing import Protocol, Self, runtime_checkable
+
+__version__ = "0.0.1.dev0"
+__all__ = ["Dimension"]
+
+@runtime_checkable
+class Dimension(Protocol):
+    def __eq__(self, other: Self, /) -> bool: ...
+    def __mul__(self, other: Self, /) -> Self: ...
+    def __div__(self, other: Self, /) -> Self: ...
+    def __pow__(self, other: int, /) -> Self: ...
