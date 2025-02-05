e8e35f28476378b976006947c8ac2fe51ae77b26
Lucas Colley <lucas.colley8@gmail.com>
2025-02-05 15:21:21 +0000

    Apply suggestions from code review
    Co-authored-by: Joren Hammudoglu <jhammudoglu@gmail.com>

--- src/dimension_api/__init__.py
+++ src/dimension_api/__init__.py
@@ -1,13 +1,13 @@
 """Dimension API."""
 
-from typing import Protocol, Self, runtime_checkable
+from typing import Final, Protocol, Self, runtime_checkable
 
-__version__ = "0.0.1.dev0"
-__all__ = ["Dimension"]
+__version__: Final = "0.0.1.dev0"
+__all__ = ["__version__", "Dimension"]
 
 @runtime_checkable
 class Dimension(Protocol):
     def __eq__(self, other: Self, /) -> bool: ...
     def __mul__(self, other: Self, /) -> Self: ...
-    def __div__(self, other: Self, /) -> Self: ...
+    def __truediv__(self, other: Self, /) -> Self: ...
     def __pow__(self, other: int, /) -> Self: ...
